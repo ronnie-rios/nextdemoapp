4995dfe3a0ef762c5f704179a54027f202f82cb8
Ronnie Rios <ronniesmail14@gmail.com>
2023-08-16 11:41:19 -0500

    iniital hero styling

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,13 @@ import Image from 'next/image'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className='hero'>
+        <div className='flex-1 pt-36 padding-x'>
+            <h1 className="hero__title">Rent a car!</h1>
+            <p className='hero__subtitle'>Car rental experience.</p>
+            
+        </div>
+    </div>
   )
 }
 
