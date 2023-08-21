399ee71528a012f5c5e2b7d099be60eddabc2b42
Ronnie Rios <ronniesmail14@gmail.com>
2023-08-20 19:07:08 -0500

    manufacturer

--- components/SearchManufacture.tsx
+++ components/SearchManufacture.tsx
@@ -1,10 +1,53 @@
-import { SearchManufactureProps } from '@/types'
-import React from 'react'
+"use client";
+import { SearchManufactureProps } from "@/types";
+import { Combobox, Transition } from "@headlessui/react";
+import Image from "next/image";
+import { useState, Fragment } from "react";
+import { manufacturers } from "@/constants";
 
-const SearchManufacture = ({ manufacture, setManufacture}: SearchManufactureProps) => {
+const SearchManufacture = ({
+  manufacture,
+  setManufacture,
+}: SearchManufactureProps) => {
+  const [query, setQuery] = useState("");
+
+  const filteredManu = query ==='' ? manufacturers : manufacturers.filter(item => (
+    item.toLowerCase().replace(/\s+/g, '').includes(query.toLowerCase().replace(/\s+/g,''))
+  ))
+
   return (
-    <div>SearchManufacture</div>
-  )
-}
+    <div className="search-manufacturer">
+      <Combobox>
+        <div className="relative w-full ">
+          <Combobox.Button className="absoulte top-[14px]">
+            <Image
+              src="/car-logo.svg"
+              width={20}
+              height={20}
+              className="ml-4"
+              alt="car logo"
+            />
+          </Combobox.Button>
+          <Combobox.Input
+            className="search-manufactuer__input"
+            placeholder="VW"
+            displayValue={(manufacture: string) => manufacture}
+            onChange={(e) => setQuery(e.target.value)}
+          />
 
-export default SearchManufacture
+          <Transition
+            as={Fragment}
+            Leave="transition ease-in duration-100"
+            LeaveFrom="opacity-100"
+            LeaveTo="opacity-0"
+            afterLeave={() => setQuery("")}
+          >
+            <Combobox.Option>{}</Combobox.Option>
+          </Transition>
+        </div>
+      </Combobox>
+    </div>
+  );
+};
+
+export default SearchManufacture;
